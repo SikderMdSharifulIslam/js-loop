75f168aba2f14bba4f6b763e630126e222001edd
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-06 14:02:06 +0600

    recap variable conditonal and array

---
+++ recap.js
@@ -0,0 +1,18 @@
+// const = value can't be changed
+// let = value can be changed
+// var = we will not use it
+
+// variable
+const amount = 200;
+
+// condition
+if(amount > 100){
+
+}
+else{
+
+}
+
+// array
+const numbers  = [12, 32, 54, 65]
+
