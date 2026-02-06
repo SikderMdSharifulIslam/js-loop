ef00be23e916336d2710c4b395c892775e858be8
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-06 23:26:46 +0600

    while loop practice

---
+++ even.js
@@ -0,0 +1,10 @@
+let num = 1;
+
+while(num <= 10)
+{
+    if(num % 2 === 0)
+    {
+        console.log(num, "is even number");
+    }
+    num++;
+}
---
+++ sum.js
@@ -0,0 +1,7 @@
+let num = 0, sum = 0;
+while(num <= 10)
+{
+    sum+=num;
+    num++;
+}
+console.log("sum", sum);
