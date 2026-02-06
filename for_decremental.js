1fc629a7f100a7a7802464605c8ffe757c68a86a
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-07 02:12:03 +0600

    decremental loops

---
+++ for_decremental.js
@@ -0,0 +1,4 @@
+for(let i = 20; i >= 10; i--)
+{
+    console.log(i);
+}
---
+++ for_sum.js
@@ -0,0 +1,8 @@
+let sum = 0;
+
+for(let num = 1; num <= 10; num++)
+{
+    sum+=num;
+}
+
+console.log("1-10 numbers sum: ", sum);
